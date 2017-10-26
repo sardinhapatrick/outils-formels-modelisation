ecf366a902d8a7542d66770987fd0666b128df56
sardinhapatrick <Patrick.Sardinha@etu.unige.ch>
2017-10-26 21:17:25 +0200

    add tp-02

---
+++ tp-02/Smokers/Sources/Smokers/main.swift
@@ -0,0 +1,164 @@
+// SARDINHA Patrick
+// Tous droits réservés.
+
+import PetriKit
+import SmokersLib
+import Foundation
+
+// Instantiate the model.
+let model = createModel()
+
+// Retrieve places model.
+guard let r  = model.places.first(where: { $0.name == "r" }),
+      let p  = model.places.first(where: { $0.name == "p" }),
+      let t  = model.places.first(where: { $0.name == "t" }),
+      let m  = model.places.first(where: { $0.name == "m" }),
+      let w1 = model.places.first(where: { $0.name == "w1" }),
+      let s1 = model.places.first(where: { $0.name == "s1" }),
+      let w2 = model.places.first(where: { $0.name == "w2" }),
+      let s2 = model.places.first(where: { $0.name == "s2" }),
+      let w3 = model.places.first(where: { $0.name == "w3" }),
+      let s3 = model.places.first(where: { $0.name == "s3" }),
+
+      let tpt  = model.transitions.first(where: { $0.name == "tpt" }),
+      let tpm  = model.transitions.first(where: { $0.name == "tpm" }),
+      let ttm  = model.transitions.first(where: { $0.name == "ttm" }),
+      let ts1  = model.transitions.first(where: { $0.name == "ts1" }),
+      let ts2  = model.transitions.first(where: { $0.name == "ts2" }),
+      let ts3  = model.transitions.first(where: { $0.name == "ts3" }),
+      let tw1  = model.transitions.first(where: { $0.name == "tw1" }),
+      let tw2  = model.transitions.first(where: { $0.name == "tw2" }),
+      let tw3  = model.transitions.first(where: { $0.name == "tw3" })
+
+else {
+    fatalError("invalid model")
+}
+
+// Nous plotons le réseau de pétri pour le visualiser en génerant un .dot
+
+let pn = PTNet(places: [r, p, t, m, w1, s1, w2, s2, w3, s3], transitions: [tpt, tpm, ttm, ts1, ts2, ts3, tw1, tw2, tw3])
+try pn.saveAsDot(to: URL(fileURLWithPath: "pn.dot"), withMarking: [r: 1, p: 0 , t: 0, m: 0, w1: 1, s1: 0, w2: 1, s2: 0, w3: 1, s3: 0])
+
+// Create the initial marking.
+
+let initialMarking: PTMarking = [r: 1, p: 0, t: 0, m: 0, w1: 1, s1: 0, w2: 1, s2: 0, w3: 1, s3: 0]
+
+// On créé la fonction countNodes afin de compter les noeuds/Etats possibles du réseau
+// Renvoie un entier
+
+func countNodes(markingGraph: MarkingGraph) -> Int {
+  // iteration
+    var seen    = [markingGraph]
+    var toVisit = [markingGraph]
+    // var result: Int = 0
+    // result += markingGraph.successors.count
+    while let current = toVisit.popLast(){
+        for (_, successor) in current.successors{
+            if !seen.contains(where: { $0 === successor }) {  // ''==='' Afin de savoir si c'est la meme classe
+                seen.append(successor)
+                toVisit.append(successor) //  Permet de voir tous les noeuds au moins une fois
+            }
+        }
+    }
+    //print(seen.count)
+    return seen.count
+}
+
+// On créé la fonction nombreFumeurs afin de savoir si 2 fumeurs différents
+// peuvent fumer en même temps
+// Renvoie un Booléen
+
+func nombreFumeurs(markingGraph: MarkingGraph) -> Bool {
+  // iteration
+    var seen    = [markingGraph]
+    var toVisit = [markingGraph]
+
+    while let current = toVisit.popLast(){
+        for (_, successor) in current.successors{
+            if !seen.contains(where: { $0 === successor }) {  // ''==='' Afin de savoir si c'est la meme classe
+                seen.append(successor)
+                toVisit.append(successor)
+                if (successor.marking[s1] == 1 && successor.marking[s2] == 1) || // Les conditions pour fumer en même temps
+                (successor.marking[s2] == 1 && successor.marking[s3] == 1) {
+                    return true
+                }
+                else if (successor.marking[s3] == 1 && successor.marking[s1] == 1){
+                    return true
+                }
+                /*switch value {
+                case value:
+
+                default:
+
+                }*/
+            }
+        }
+    }
+    return false // Sinon
+}
+
+
+// On créé la fonction nombreIngredients afin de savoir si un même ingrédient peut
+// être deux fois présents sur la table (en même temps)
+// Renvoie un Booléen
+
+func nombreIngredients(markingGraph: MarkingGraph) -> Bool {
+      // iteration
+    var seen    = [markingGraph]
+    var toVisit = [markingGraph]
+
+    while let current = toVisit.popLast(){
+        for (_, successor) in current.successors{
+            if !seen.contains(where: { $0 === successor }) {  // ''==='' Afin de savoir si c'est la meme classe
+                seen.append(successor)
+                toVisit.append(successor)
+                if (successor.marking[p] == 2){  // Conditions pour qu'un ingrédient soit 2 fois sur la table
+                    return true
+                }
+                else if (successor.marking[m] == 2){
+                    return true
+                }
+                else if (successor.marking[t] == 2){
+                    return true
+                }
+            }
+        }
+    }
+    return false  // Sinon
+}
+
+
+// Create the marking graph (if possible).
+
+if let markingGraph = model.markingGraph(from: initialMarking) {
+    // Write here the code necessary to answer questions of Exercise 4.
+    print("\n ============ TP 02 ==============")
+    print(" ======= Analyse du modèle =======\n")
+
+    // On print le nombre d'Etats possible du réseau
+    var a = countNodes(markingGraph : markingGraph)
+    print("Il y a", a,"états différents pour ce réseau !")
+    //print(a)
+
+    //var b = nombreFumeurs(markingGraph: markingGraph)
+    //print(b)
+
+    // Boucle if pour savoir si deux fumeurs différents peuvent fumer en même temps
+    if nombreFumeurs(markingGraph: markingGraph) == true {
+        print("Deux fumeurs différents peuvent fumer en même temps !")
+    }
+    else {
+        print("Il est impossible que deux fumeurs différents puissent fumer en même temps !")
+    }
+
+    //var c = nombreIngredients(markingGraph: markingGraph)
+    //print(c)
+
+    // Boucle if pour savoir si un même ingrédient peut être deux fois sur la table en même temps
+    if nombreIngredients(markingGraph: markingGraph) == true {
+        print("Un même ingrédient peut se retrouver deux fois en même temps sur la table !")
+    }
+    else {
+        print("Un même ingrédient ne peut pas se retrouver deux fois en même temps sur la table !")
+    }
+}
---
+++ tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
@@ -0,0 +1,61 @@
+// SARDINHA Patrick
+// Tous droits réservés.
+
+import PetriKit
+
+public class MarkingGraph {
+
+    public let marking   : PTMarking
+    public var successors: [PTTransition: MarkingGraph]
+
+    public init(marking: PTMarking, successors: [PTTransition: MarkingGraph] = [:]) {
+        self.marking    = marking
+        self.successors = successors
+    }
+
+}
+
+public extension PTNet {
+
+      public func markingGraph(from marking: PTMarking) -> MarkingGraph? {
+        // Write here the implementation of the marking graph generation.
+
+        var Etat = MarkingGraph(marking: marking) // On construit l'état initial
+        //print(Etat)
+        var ProchainEtat = [MarkingGraph]() // On créé le prochain état à analyser. Initialisé à vide pour le moment
+        ProchainEtat.insert(Etat, at:0) // On met l'Etat initial dans ProchainEtat
+        //print(ProchainEtat)
+        var EtatVisite = [MarkingGraph]()// On définie les états visités, vide pour le moment
+        EtatVisite.insert(Etat, at:0) // On insert l'Etat initial dedans
+        //print(EtatVisite)
+        let m = marking
+
+        // On créé une boucle afin de parcourir les graphes de marquages
+
+          while let EtatActuel = ProchainEtat.popLast(){ // while !(ProchainEtat.isEmpty){
+                                                        // var EtatActuel = ProchainEtat.popLast()
+          EtatVisite.append(EtatActuel) // On append la liste afin d'ajouter les MarkingGraph visités
+          for Transi in self.transitions{ // On fait les transitions une par une
+              if let fireable = Transi.fire(from: EtatActuel.marking){ // Teste si tirable
+                  if ProchainEtat.contains(where: {$0.marking == fireable}){ // Pour les états pas encore testés
+                                                                            //  Contains nous permet de savoir si un élément satisfait le prédicat
+                      EtatActuel.successors[Transi] = ProchainEtat.first(where: {$0.marking == fireable}) // Le prochain état atteignable par Transi dans ProchainEtat?
+                                                                                                         // .first nous permet d'obtenir le premier élément qui satisfait le prédicat
+                  }
+                  else if EtatVisite.contains(where: {$0.marking == fireable}) { // Pour les états déjà testés
+                      EtatActuel.successors[Transi] = EtatVisite.first(where: {$0.marking == fireable}) // Le prochain état atteignable par Transi dans EtatVisite?
+                  }
+                  else {
+                      var NouvelEtat = MarkingGraph(marking: fireable) // Sinon on créé un nouvel état
+                      ProchainEtat.append(NouvelEtat) // Et append
+                      EtatActuel.successors[Transi] = NouvelEtat
+
+                  }
+              }
+
+          }
+
+        }
+      return Etat // On renvoit les Etats
+     }
+}
