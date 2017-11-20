4ad75e29cd04d0f80423b7c939721f9107adf601
sardinhapatrick <Patrick.Sardinha@etu.unige.ch>
2017-11-20 14:36:35 +0100

    repull

--- tp-04/Philosophers/Sources/Philosophers/main.swift
+++ tp-04/Philosophers/Sources/Philosophers/main.swift
@@ -16,26 +16,29 @@ do {
 
     func g(binding: PredicateTransition<C>.Binding) -> C {
         switch binding["x"]! {
-        case .b: return .v
-        case .v: return .b
-        case .o: return .o
+        case .b: return .v // g(b) = v
+        case .v: return .b // g(v)  = b
+        case .o: return .o // g(o) = o
         }
     }
 
     let t1 = PredicateTransition<C>(
-        preconditions: [
+        preconditions: [                // label -> soit variable soit fonction
             PredicateArc(place: "p1", label: [.variable("x")]),
+            // de la place p1 veut un label (multiset = tab non trié) qui a la var x
         ],
         postconditions: [
             PredicateArc(place: "p2", label: [.function(g)]),
+            // label avec fonction g
         ])
 
+// fabrique marquage initial
     let m0: PredicateNet<C>.MarkingType = ["p1": [.b, .b, .v, .v, .b, .o], "p2": []]
-    guard let m1 = t1.fire(from: m0, with: ["x": .b]) else {
+    guard let m1 = t1.fire(from: m0, with: ["x": .b]) else { // tirer transition mais doit donner le binding: x vaut b --> T1/x=b
         fatalError("Failed to fire.")
     }
     print(m1)
-    guard let m2 = t1.fire(from: m1, with: ["x": .v]) else {
+    guard let m2 = t1.fire(from: m1, with: ["x": .v]) else { // idem
         fatalError("Failed to fire.")
     }
     print(m2)
@@ -48,4 +51,43 @@ do {
     for m in philosophers.simulation(from: philosophers.initialMarking!).prefix(10) {
         print(m)
     }
+}
+
+// Exo vendredi 17.11.2017
+
+do {
+  // Dom(Ingredients)
+    enum Ingredients{
+        case p,t,m
+    }
+    enum Smokers {
+        case mia,tom,bob
+    }
+    enum Referee {
+        case rob
+    }
+    enum Types {
+        case ingredients(Ingredients)
+        case smokers(Smokers)
+        case referee(Referee)
+    }
+
+    let s = PredicateTransition<Types>{
+        preconditions: [                // label -> soit variable soit fonction
+            PredicateArc(place: "i", label: [.variable("x"),.variable("y")]),
+            PredicateArc(place: "s", label: [.variable("s")]),
+        ],
+        postconditions: [
+            PredicateArc(place: "r", label: [.function({_ in .referee(.rob)})]),
+            PredicateArc(place: "w", label: [.function("s")]),
+        ],
+      condition       : [{ binding init() {
+          guard case let .smokers(s)     = binding["s"]!,
+                case let .ingredients(x) = binding["x"]!,
+                case let .ingredients(y) = binding["y"]!
+          else
+      }}])
+
+    }
+  }
 }
--- tp-04/Philosophers/Sources/PhilosophersLib/PredicateNet.swift
+++ tp-04/Philosophers/Sources/PhilosophersLib/PredicateNet.swift
@@ -77,7 +77,7 @@ public class PredicateTransition<T: Equatable> {
         var inboundVariables: Set<Variable> = []
 
         for arc in preconditions {
-            // Make sure the a doesn't appear twice as a precondition.
+            // Make sure the a doesn't appear twice as a precondition. -> 1 place - 1 condition
             guard !inboundPlaces.contains(arc.place) else {
                 preconditionFailure("Place '\(arc.place)' appear twice as precondition.")
             }
@@ -151,7 +151,7 @@ public class PredicateTransition<T: Equatable> {
     ///   to pick `1` once again. Now if we check the second arrangement, we'll bind `x` to `2`
     ///   and `y` to `1` before moving to `p1`. But as we won't be able to match `2` in the tokens
     ///   of `p1`, we'll reject the binding and move to the next arrangement.
-    public func fireableBingings(from marking: PredicateNet<T>.MarkingType) -> [Binding] {
+    public func fireableBingings(from marking: PredicateNet<T>.MarkingType) -> [Binding] { // -> toutes les combinaisons de binding
         // Sort the places so we always bind their variables in the same order.
         let variables    = self.inboundVariables()
         let sortedPlaces = variables.keys.sorted()
@@ -207,7 +207,7 @@ public class PredicateTransition<T: Equatable> {
     ///
     /// - Note: If the transition isn't fireable with the provided marking and binding, the method
     ///   will return a nil value.
-    public func fire(from marking: PredicateNet<T>.MarkingType, with binding: Binding)
+    public func fire(from marking: PredicateNet<T>.MarkingType, with binding: Binding) // prend un marquage et un binding
         -> PredicateNet<T>.MarkingType?
     {
         // Check whether the provided binding is valid.
@@ -225,6 +225,7 @@ public class PredicateTransition<T: Equatable> {
         for condition in self.conditions {
             guard condition(binding) else { return nil }
         }
+        //--> isFirable depuis func fire
 
         var result = marking
 
@@ -247,7 +248,7 @@ public class PredicateTransition<T: Equatable> {
                     // checked that postconditions aren't labeled with free variables.
                     result[arc.place]!.append(binding[v]!)
                 case .function(let f):
-                    result[arc.place]!.append(f(binding))
+                    result[arc.place]!.append(f(binding)) // si c'est une fonction alors on l'applle
                 }
             }
         }
@@ -332,7 +333,7 @@ public class PredicateArc<T: Equatable>: Hashable {
 
 }
 
-public indirect enum PredicateLabel<T: Equatable> {
+public indirect enum PredicateLabel<T: Equatable> { // T --> n'importe quel ens de T
 
     case variable(Variable)
     case function((PredicateTransition<T>.Binding) -> T)
